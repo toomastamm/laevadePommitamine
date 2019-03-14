4369201b244f29ccfff145061b4880ab6a4f9e2f
Toomas Tamm <toomastmm@gmail.com>
2019-03-14 13:22:43 +0200

    Algne paigutaLaev funktsioon

--- src/Mängija.java
+++ src/Mängija.java
@@ -5,4 +5,18 @@ public class Mängija {
         this.mänguväli = new Väli(pikkus);
     }
 
+
+    void paiguaLaev(String suund, int pikkus, int x, int y) {
+        int delta_x = 0;
+        int delta_y = 0;
+        if (suund == "alla") {
+            delta_y = 1;
+        } else if (suund == "paremale") {
+            delta_x = 1;
+        }
+
+        for (int i = 0; i < pikkus; i++) {
+            mänguväli.määraSisu(x + i*delta_x, y + i*delta_y, "X");
+        }
+    }
 }
