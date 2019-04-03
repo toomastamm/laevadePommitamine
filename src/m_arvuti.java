ba55481366c8621ae028cd087f39f8284be20048
Toomas Tamm <toomastmm@gmail.com>
2019-04-03 21:44:26 +0300

    Nüüd pommitab arvuti korrektselt välju, mida on mõtet proovida

--- src/m_arvuti.java
+++ src/m_arvuti.java
@@ -9,19 +9,18 @@ public class m_arvuti extends Mängija {
         int x;
         int y;
         String suund;
-        int suuna = (int)(Math.random()*2);
-        if(suuna == 0) {
+        int suuna = (int) (Math.random() * 2);
+        if (suuna == 0) {
             suund = "paremale";
             y = (int) (Math.random() * (10 - pikkus));
             x = (int) (Math.random() * 10);
-        }
-        else {
+        } else {
             suund = "alla";
             y = (int) (Math.random() * 10);
             x = (int) (Math.random() * (10 - pikkus));
 
         }
-            boolean paigutus = prooviPaigutada(x, y, pikkus, suund);
+        boolean paigutus = prooviPaigutada(x, y, pikkus, suund);
 
         if (!paigutus) {
             System.out.println("Paigutamine ebaõnnestus!");
@@ -33,20 +32,16 @@ public class m_arvuti extends Mängija {
     @Override
     boolean lase(Mängija vastane) {
         vastane.väljastaVäliVastasele();
-        boolean sobiv = false;
-        int y = (int)(Math.random()*10);
-        int x = (int)(Math.random()*10);
-        while (sobiv = false){
 
-        if((vastane.getMänguväli().getSisu(x,y)).equals("tavaline")){
-            sobiv = true;
+        while (true) {
+            int y = (int) (Math.random() * 10);
+            int x = (int) (Math.random() * 10);
+
+            if (vastane.getMänguväli().getSisu(x, y).equals(Väli.laev) || vastane.getMänguväli().getSisu(x, y).equals(Väli.tavaline)) {
+                return vastane.vastaseLask(x, y);
+            }
+
         }
-        else {
-            y = (int)(Math.random()*10);
-            x = (int)(Math.random()*10);
-        }
-        }
-        return vastane.vastaseLask(y, x);
     }
 }
 
